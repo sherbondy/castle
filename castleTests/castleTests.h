0d697d313a7da3cb39bd38835ca4cf229dc7fe4c
Ethan Sherbondy <sherbondye@gmail.com>
2012-03-14 17:41:15 -0400

    Initial Commit

---
+++ castleTests/castleTests.h
@@ -0,0 +1,13 @@
+//
+//  castleTests.h
+//  castleTests
+//
+//  Created by Ethan Sherbondy on 3/14/12.
+//  Copyright (c) 2012 MIT. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface castleTests : SenTestCase
+
+@end
---
+++ castleTests/castleTests.m
@@ -0,0 +1,32 @@
+//
+//  castleTests.m
+//  castleTests
+//
+//  Created by Ethan Sherbondy on 3/14/12.
+//  Copyright (c) 2012 MIT. All rights reserved.
+//
+
+#import "castleTests.h"
+
+@implementation castleTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in castleTests");
+}
+
+@end
